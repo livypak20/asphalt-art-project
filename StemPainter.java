2d50b4e519b69c2fc8210a7fcad58c244b12d7fd
livypak <livypak@gmail.com>
2025-09-26 21:48:01 -0700

    Update StemPainter.java

--- StemPainter.java
+++ StemPainter.java
@@ -2,6 +2,9 @@ import org.code.neighborhood.*;
 
 public class StemPainter extends PainterPlus{
 
+   /*
+  *moves painter that paints the stem to the bottom middle of the screen
+  */
   public void moveToBottomMiddle(){
     for(int i =0; i < 8; i++){
       move();
@@ -11,6 +14,9 @@ public class StemPainter extends PainterPlus{
     turnAround();
   }
 
+   /*
+  *moves painter upward while painting a color to make the stem structure
+  */
   public void paintStem(String color){
     setPaint(8);
     for(int i = 0; i < 8; i++){
